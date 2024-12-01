11d235e6855cb659f484c2c06cfe21b9c7c65f01
Nemanja Stojanovic <feemail042@gmail.com>
2024-12-01 13:54:54 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-orange-500 font-bold">React JS</h1>
+    </div>
+  );
+};
+
+export default App;
