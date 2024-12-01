c541a184f2cb71932ca58efdac55ac83aa7154b1
Nemanja Stojanovic <feemail042@gmail.com>
2024-12-01 22:36:24 +0100

    default colors added ti tsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-5xl text-orange-500 font-bold font-zentry">
+      <h1 className="text-5xl text-violet-300 font-bold font-zentry">
         React JS
       </h1>
     </div>
