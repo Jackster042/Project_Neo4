1e8cb55043ea8209e553abaec836f5375be927ae
Nemanja Stojanovic <feemail042@gmail.com>
2024-12-01 22:22:20 +0100

    added default font settings in tailwind.config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-5xl text-orange-500 font-bold">React JS</h1>
+      <h1 className="text-5xl text-orange-500 font-bold font-zentry">
+        React JS
+      </h1>
     </div>
   );
 };
