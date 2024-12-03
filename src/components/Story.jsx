8bd446fc7983a41eb194b8af769748e69a88d9cd
Nemanja Stojanovic <feemail042@gmail.com>
2024-12-03 23:07:38 +0100

    Story section done

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,12 +1,105 @@
-import React from "react";
+import React, { useRef } from "react";
+
+import Button from "./Button";
+import AnimatedTitle from "./AnimatedTitle";
+import RoundedCorners from "./RoundedCorners";
+
+import gsap from "gsap";
 
 const Story = () => {
+  const frameRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    // console.log(`clientX : ${clientX}, clientY : ${clientY}`);
+
+    const element = frameRef.current;
+    // console.log(element.getBoundingClientRect());
+
+    if (!element) return;
+
+    // CALCULATE CENTER OF THE IMAGE
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+    // console.log(`X :${x}, Y: ${y}`);
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    // CALCULATE ROTATION VALUES BASED ON THE MOUSE POSITION RELATIVE TO THE CENTER
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseLeave = (e) => {
+    const element = frameRef.current;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: "power1.inOut",
+    });
+  };
+
   return (
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
-          Welcome to Zentry
-        </h2>
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="The st<b>o</b>ry of <br /> a hidden real<b></b>m "
+            sectionId="#story"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+
+          {/* IMAGE CONTAINER */}
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  src="/img/entrance.webp"
+                  alt="entrance"
+                  className="object-contain"
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                />
+              </div>
+            </div>
+
+            <RoundedCorners />
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-button"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
