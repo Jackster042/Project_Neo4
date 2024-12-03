aa21609d707cbfdc85363f785a97506eaf9559e0
Nemanja Stojanovic <feemail042@gmail.com>
2024-12-03 16:22:01 +0100

    bento grid layout done || TODO BentoTilt component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { useState, useRef } from "react";
+
+import { TiLocationArrow } from "react-icons/ti";
+
+//TODO BENTO TILT
 
 // BENTO CARD SETUP !! NO NEED FOR COMPONENT COZ IT WILL BE USED ONLY HERE
 export const BentoCard = ({ src, title, description }) => {
@@ -53,6 +57,66 @@ const Features = () => {
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
             isComingSoon
           />
+        </div>
+
+        {/* GRID LAYOUT */}
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              src="videos/feature-2.mp4"
+              title={
+                <>
+                  zig<b>m</b>a
+                </>
+              }
+              description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
+            />
+          </div>
+
+          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="videos/feature-3.mp4"
+              title={
+                <>
+                  n<b>e</b>xus
+                </>
+              }
+              description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+            />
+          </div>
+
+          <div className="bento-tilt_1 row-span-1 me-14 md:col-span-1 md:me-0">
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title={
+                <>
+                  az<b>u</b>l
+                </>
+              }
+              description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+            />
+          </div>
+
+          <div className="bento-tilt_2">
+            <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+              <h1 className="bento-title special-font max-w-64 font-black">
+                M<b>o</b>re
+                <br /> co<b>m</b>ing
+                <br /> so<b>o</b>n
+              </h1>
+              <TiLocationArrow className="m-5 scale-[5] self-end" />
+            </div>
+          </div>
+
+          <div className="bento-tilt_2">
+            <video
+              src="videos/feature-5.mp4"
+              loop
+              muted
+              autoPlay
+              className="size-full object-cover object-center"
+            />
+          </div>
         </div>
       </div>
     </section>
